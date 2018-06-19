b67873f8138e96909f0c563dc908c69cd51ea014
test <marcel.klomann@informatik.hs-fulda.de>
2018-06-19 12:25:12 +0200

    added some materials and a test script

---
+++ Test/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+    public Vector3 StartPosition = new Vector3(0.0f, 0.5f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float time = UnityEngine.Time.time;
+        float offset = Mathf.Sin(time);
+
+        this.transform.localPosition = this.StartPosition + Vector3.up * offset;
+	}
+}
